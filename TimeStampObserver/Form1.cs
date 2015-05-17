0a6be06f77a9c9ab2e9991b1cbaa69107a591c08
Hiro <seasolt.bluesky13@gmail.com>
2015-05-17 20:46:58 +0900

    Add Working Directory Setting.

--- TimeStampObserver/Form1.cs
+++ TimeStampObserver/Form1.cs
@@ -13,6 +13,7 @@ namespace TimeStampObserver
     public partial class Form1 : Form
     {
         OpenFileDialog ofd1, ofd2;
+        FolderBrowserDialog fbd;
         bool working;
 
         public Form1()
@@ -29,6 +30,10 @@ namespace TimeStampObserver
             ofd2.Title = "実行するプログラムを選択してください";
             ofd2.RestoreDirectory = true;
 
+            fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = @"C:\";
+            fbd.Description = "ワーキングディレクトリを設定してください";
+
             working = false;
         }
 
@@ -42,34 +47,62 @@ namespace TimeStampObserver
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (ofd1.ShowDialog() == DialogResult.OK)
+            if (fbd.ShowDialog() == DialogResult.OK)
             {
-                this.textBox1.Text = ofd1.FileName;
+                this.textBox1.Text = fbd.SelectedPath;
+                this.ofd1.InitialDirectory = fbd.SelectedPath;
+                this.ofd2.InitialDirectory = fbd.SelectedPath;
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (ofd2.ShowDialog() == DialogResult.OK)
+            if (ofd1.ShowDialog() == DialogResult.OK)
             {
-                this.textBox2.Text = ofd2.FileName;
+                if (ofd1.FileName.IndexOf(' ') == -1)
+                {
+                    this.textBox2.Text = ofd1.FileName;
+                }
+                else
+                {
+                    this.textBox2.Text = "\"" + ofd1.FileName + "\"";
+                }
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            if (ofd2.ShowDialog() == DialogResult.OK)
+            {
+                if (ofd2.FileName.IndexOf(' ') == -1)
+                {
+                    this.textBox3.Text = ofd2.FileName;
+                }
+                else
+                {
+                    this.textBox3.Text = "\"" + ofd2.FileName + "\"";
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
             if (working)
             {
-                this.button3.Text = "実行(&R)";
+                this.button4.Text = "実行(&R)";
+                this.button1.Enabled = true;
+                this.button2.Enabled = true;
+                this.button3.Enabled = true;
                 this.textBox1.Enabled = true;
                 this.textBox2.Enabled = true;
+                this.textBox3.Enabled = true;
                 working = false;
             }
             else
             {
                 string file = textBox1.Text;
                 string cmdln = textBox2.Text;
-                cmdln = cmdln.Replace("$F",file);
+                cmdln = cmdln.Replace("$F", file);
                 cmdln = cmdln.Replace("$$", "$");
                 string[] commands = new List<string>(cmdln.Split(' ')).ToArray();
 
@@ -79,9 +112,13 @@ namespace TimeStampObserver
                     MessageBox.Show(c);
                 }
 
-                this.button3.Text = "停止(&S)";
+                this.button4.Text = "停止(&S)";
+                this.button1.Enabled = false;
+                this.button2.Enabled = false;
+                this.button3.Enabled = false;
                 this.textBox1.Enabled = false;
                 this.textBox2.Enabled = false;
+                this.textBox3.Enabled = false;
                 working = true;
             }
         }
